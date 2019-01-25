909df5d5449bd7645996ff17f8d54e8efd5c6e1a
Deng Xiaolong <xiaolong.deng@ncsi.com.cn>
2019-01-25 12:51:56 +0800

    remove some useless books

--- 实用工具/promise/promise.js
+++
@@ -1,9 +0,0 @@
-var Promise = require('promise');
-
-// Promise.resolve(43).then(function(value){
-// 	console.log(value);
-// });
-
-var promise = new Promise(function(resolve, reject){
-	reject(new Error('出错了'));
-})
